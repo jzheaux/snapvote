86949732f8523191a4c1223d0b060d708d91ad2f
Josh Cummings <josh.cummings@gmail.com>
2015-10-05 14:37:43 -0600

    Changed req.remoteAddress to the correct req.connection.remoteAddress

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ server.get("/", function (req, res) {
 
 server.pre(function(req, res, next) {
 	var agent = req.headers["user-agent"];
-	var address = req.remoteAddress;
+	var address = req.connection.remoteAddress;
 	req.hash = new Buffer(agent + address).toString('base64');
 	next();
 });
